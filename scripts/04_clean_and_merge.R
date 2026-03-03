2b85dd6a6dd9158e3ef24d06514f5a0279581608
Trent Pfeiffer <Trentpf@hotmail.com>
2026-03-03 11:47:02 -0500

    Add data analysis and variable naming fix in script 4

--- scripts/04_clean_and_merge.R
+++ scripts/04_clean_and_merge.R
@@ -8,6 +8,6 @@ wb_clean <- wb_raw %>%
   filter(!is.na(gdp_per_capita), !is.na(life_expectancy)) %>%
   filter(gdp_per_capita > 0) %>%
   mutate(log_gdp = log(gdp_per_capita)) %>%
-  select(iso2c, country, year, region, income, gdp_per_capita_log_gdp, life_expectancy)
+  select(iso2c, country, year, region, income, gdp_per_capita, log_gdp, life_expectancy)
 
 write_csv(wb_clean, "data_clean/wb_gdp_life_clean.csv")
